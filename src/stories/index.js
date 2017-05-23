23a9ea962cedbdfaf4d921b74b7e400753944b20
Alexandre BODIN <bodin.alex@gmail.com>
2017-05-23 23:36:34 +0200

    make all addWithJSX

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,22 +5,14 @@ import JSXAddon from 'storybook-addon-jsx';
 
 setAddon(JSXAddon);
 
-const stories = storiesOf('Storybook Knobs', module);
-
-// Add the `withKnobs` decorator to add knobs support to your stories.
-// You can also configure `withKnobs` as a global decorator.
-stories.addDecorator(withKnobs);
-
-// Knobs for React props
-stories.add('with a button', () => (
-  <button disabled={boolean('Disabled', false)}>
-    {text('Label', 'Hello Button')}
-  </button>
-))
-
-// Knobs as dynamic variables.
-stories
-  .add('as dynamic variables', () => {
+storiesOf('Storybook Knobs', module)
+  .addDecorator(withKnobs)
+  .addWithJSX('with a button', () => (
+    <button disabled={boolean('Disabled', false)}>
+      {text('Label', 'Hello Button')}
+    </button>
+  ))
+  .addWithJSX('as dynamic variables', () => {
     const name = text('Name', 'Arunoda Susiripala');
     const age = number('Age', 89);
 
@@ -34,9 +26,6 @@ stories
     const content = `I am ${name} and I'm ${age} years old.`;
     return (<div>{content}</div>);
   })
-
-
-
-stories.addWithJSX('truc', () => {
-  return (<div> hello </div>)
-})
+  .addWithJSX('truc', () => {
+    return (<div> hello </div>)
+  })
